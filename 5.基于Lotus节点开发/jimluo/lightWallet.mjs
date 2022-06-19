7d5333cad661b248c76d4d1d5416758ad421abdf
root <root@jimluo>
2022-06-19 19:48:06 +0800

    homework 5 : jimluo

---
+++ 5.基于Lotus节点开发/jimluo/lightWallet.mjs
@@ -0,0 +1,56 @@
+import { HttpJsonRpcConnector, LightWalletProvider, LotusClient } from "filecoin.js"
+import BigNumber from "bignumber.js";
+import { config } from "./config.mjs"
+
+// export NODE_OPTIONS=--openssl-legacy-provider  # for new nodejs version
+const glifNodeConnection = new HttpJsonRpcConnector({ url: config.glifNodeUrl });
+const glifClient = new LotusClient(glifNodeConnection);
+
+async function createLightwallet() {
+    try {
+        const lightWallet = new LightWalletProvider(glifClient, () => { return config.glifPwd }, 'test');
+        const mnemonic = await lightWallet.createLightWallet(config.glifPwd);
+        const from = await lightWallet.getDefaultAddress();
+        // const to = await lightWallet.newAddress();
+        return { wallet: lightWallet, mnem: mnemonic, from: from };//, to: to};
+        // const encryptedWallet = lightWallet.keystore.serialize();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function sendFromLightWallet(mnemonic, from, to, value) {
+    try {
+        const wallet = new LightWalletProvider(glifClient, () => { return config.glifPwd }, 'test');
+        await wallet.recoverLightWallet(mnemonic, config.glifPwd);
+        const message = await wallet.createMessage({
+            From: from, To: to, Value: value
+        });
+        const signedMessage = await wallet.signMessage(message);
+        const msgCid = await wallet.sendSignedMessage(signedMessage);
+        return msgCid;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const wallet = await createLightwallet();
+console.log("1. create wallet mnemonic:", wallet.mnem);
+console.log("2. create wallet from/to:", wallet.from);//, wallet.to);
+
+const fromAccount = "t1efti56cnu4vbugmwir3ge4kvcpvwasksdivf7qq"
+const toAccount = "t1d746skawojyfh2xnxwpiwya5gidhwtvezih66si"
+
+const cid = await sendFromLightWallet("water intact ten act lizard toe capable once inspire peace napkin bullet",
+    fromAccount, toAccount, new BigNumber(10000000000000000000));
+// const cid = await sendFromLightWallet(wallet.mnem, 
+//    wallet.from, wallet.to, new BigNumber(10000000000000000000));
+console.log("3. transaction  cid:", cid);
+
+// console.log
+// ❯ node lightWallet.mjs
+// 1. create wallet mnemonic: second path expire polar term stadium wink celery clarify effort obscure release
+// 2. create wallet from/to: t1sfnhgry7dnsrds2ww6qci2gt22naj4rktgdyqaa3. transaction  cid: {
+//     '/': 'bafy2bzacea3mypsc5c6u532ntdw2eqhtfwwkn7m3whxfh3mjeqbav3uo3mtim'
+//   }
---
+++ 5.基于Lotus节点开发/jimluo/lotusWallet.mjs
@@ -0,0 +1,60 @@
+
+
+import { HttpJsonRpcConnector, LotusWalletProvider, LotusClient } from "filecoin.js"
+import BigNumber from "bignumber.js";
+import { config } from "./config.mjs"
+
+const conn = new HttpJsonRpcConnector({ url: config.url, token: config.token });
+const client = new LotusClient(conn);
+const wallet = new LotusWalletProvider(client);
+
+async function getWalletAccountFromTo() {
+    try {
+        const from =  await wallet.getDefaultAddress();
+        const to = await wallet.newAddress(); 
+        return {from: from, to: to};
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function transferFiL(from, to, value) {
+    try {
+        const msgResult = await wallet.sendMessage({
+            From: from, To: to, value: value,
+        });
+        return msgResult;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function queryBalance(account) {
+    try {
+        const balance = await wallet.getBalance(account);
+        return balance;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const account = await getWalletAccountFromTo();
+console.log("1. my wallet default/new(from/to): ", account.from, account.to);
+
+const result = await transferFiL(account.from, account.to, new BigNumber(10000000000000000000))
+console.log("2. transfer FIL message CID:", result.CID);
+
+const fromBalance = await queryBalance(account.from);
+const toBalance = await queryBalance(account.to);
+console.log("3. account from balance:", account.from, fromBalance);
+console.log("4. account to balance:", account.to, toBalance);
+
+// console.log
+// ❯ node lotusWallet.mjs
+// 1. my wallet default/new(from/to):  t1t66l2theuii4cmvbo4serwbr6fgrrye6xgqljfy t1xdztipm53zpdxxw76gpewwzedco76ucbtqbt6da
+// 2. transfer FIL message CID: {
+//   '/': 'bafy2bzacebxuc7mwlxsejelup4eljmeaz7ow4deubhl37awlm3szrwtvewnrg'
+// }
+// 3. account from balance: t1t66l2theuii4cmvbo4serwbr6fgrrye6xgqljfy 449999998619530296754
+// 4. account to balance: t1xdztipm53zpdxxw76gpewwzedco76ucbtqbt6da 0
